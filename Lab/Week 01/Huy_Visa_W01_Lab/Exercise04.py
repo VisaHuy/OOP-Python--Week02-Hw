8510df800f025cbbc2b77b7422a421d74c20c088
Huy Visa <visadekh@gmail.com>
2025-01-27 20:47:12 +0700

    Delete Lab directory

--- Lab/Week 01/Huy_Visa_W01_Lab/Exercise04.py
+++
@@ -1,7 +0,0 @@
-x = input()
-a = int(x)
-factorial = 1
-while(a > 0):
-    factorial *= a
-    a -= 1
-print(factorial) 
--- Lab/Week 02/Huy_Visa_W02_Lab/Exercise02.py
+++
@@ -1,9 +0,0 @@
-#Create a list
-myList = []
-for x in range(21):
-    if x % 2 == 0:
-        myList.append(x)
-print(myList)
-#Create a square list using List Comprehension
-newList = [x ** 2 for x in myList] #x ** 2 mean x X x
-print(newList)
